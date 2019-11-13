4b6ea08e35a55a2158803ec0c251a05e5a89bb29
felipefln <felipecarvalho_22_@hotmail.com>
2019-11-13 19:19:50 -0300

    Fix: Correçao bug web cam captura

--- src/components/CriarCliente.js
+++ src/components/CriarCliente.js
@@ -11,48 +11,38 @@ export default function CriarCliente({ history }) {
     const [email, setEmail] = useState('')
     const [cpf, setCpf] = useState('')
     const [telefone, setTelefone] = useState('')
-    
+    const [web, setWeb] = useState(false)
+    const [numero, setNumero ] = useState(3)
+    const webcamRef = React.useRef(null)
+
+    function dataURLtoFile(dataurl, filename) {
+        var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+            bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+        while(n--){
+            u8arr[n] = bstr.charCodeAt(n);
+        }
+        return new File([u8arr], filename, {type:mime});
+    }
     // preview da imagem escolhido via input
     const preview = useMemo(() =>
+    
     imagem
       ? URL.createObjectURL(imagem)
       : null
     , [imagem])
 
     // capturando imagem da camera
-    function webCameraCapture () {
-        const setRef = webcam => {
-            this.webcam = webcam;
-          };
-
-        const videoConstraints = {
-            width: 1280,
-            height: 720,
-            facingMode: "user"
-        };
-        
-        const capture = () => {
-            const imageSrc = this.webcam.getScreenshot();
-            console.log(imageSrc)
-        };
-
-        console.log('falta retornar agora pai ')
-
-        return (
-            <div>
-              <Webcam
-                audio={false}
-                height={350}
-                ref={setRef}
-                screenshotFormat="image/jpeg"
-                width={350}
-                videoConstraints={videoConstraints}
-              />
-              <Button onClick={capture}>Capture photo</Button>
-            </div>
-            );
-          }
-        
+    const capture = React.useCallback(
+        () => {
+            const imageSrc = webcamRef.current.getScreenshot();
+            setNumero(Math.floor(Math.random() * 2000));
+            const fileName  = `capture${numero}.png`
+            const fileUrl = dataURLtoFile(imageSrc, fileName)
+            setImagem(fileUrl)
+          },
+          [numero]
+    );
+                
     // funçao async do form, montando o data pra enviar pra API
     async function handleSubmit(event) {
         event.preventDefault();
@@ -76,18 +66,45 @@ export default function CriarCliente({ history }) {
     return (
         <div className='containerForm'>
             <form onSubmit={handleSubmit}>
-                <label id='imagem' style={{backgroundImage: `url(${preview})`}}>
+                {
+                    web ?
+                    <label id='imagem' style={{backgroundImage: `url(${preview})`}}>
+                         <Webcam
+                            audio={false}
+                            height={400}
+                            ref={webcamRef}
+                            screenshotFormat="image/jpeg"
+                            width={425}
+                            />
+                            <Button onClick={capture}>
+                    
+                                
+                                <img src={camera} alt="img-thun"/>
+                            
+                            </Button>
+                    
+                    
+                    
+                    </label> 
+                    :
+                    <label id='imagem' style={{backgroundImage: `url(${preview})` }}>
+               
                     <input type="file"  onChange={event => setImagem(event.target.files[0])}/>
-                    
-                    
-                    
                 </label>
-                <Button outline color="success" onClick={webCameraCapture}>
-                    
-                        Capture sua imagem pela webcam
-                        <img src={camera} alt="img-thun"/>
-                    
-                </Button>
+                
+                }
+                {
+                    web ? 
+                    false
+                     :
+                     <Button outline color="success" onClick={() => setWeb(true)}>
+                
+                     Capture sua imagem pela webcam
+                     <img src={camera} alt="img-thun"/>
+                 
+                     </Button>
+                }
+                 
                 <div className="labels">
                         <label htmlFor="nome">Nome *</label>
                         <input 
